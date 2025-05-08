55289bc3496f2230f3ac6fbdb5d30ce8c0cfda61
ismaelpaul <ismaafp@gmail.com>
2025-05-08 18:41:12 +0100

    Refactor folder structure and pass autoFocus and disabled as props

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,14 +1,21 @@
 'use client';
 
 import { useEffect, useRef } from 'react';
-import { useFormSubmitOnEnter } from '@/hooks/useFormSubmittionOnEnter';
+import { useFormSubmitOnEnter } from '@/hooks/useFormSubmittionOnEnter/useFormSubmittionOnEnter';
 
 type InputProps = {
 	message: string;
 	setMessage: React.Dispatch<React.SetStateAction<string>>;
+	autoFocus?: boolean;
+	disabled?: boolean;
 };
 
-export default function Input({ message, setMessage }: InputProps) {
+export default function Input({
+	message,
+	setMessage,
+	autoFocus,
+	disabled,
+}: InputProps) {
 	const textareaRef = useRef<HTMLTextAreaElement | null>(null);
 
 	const handleKeyDown = useFormSubmitOnEnter(message, textareaRef);
@@ -27,8 +34,10 @@ export default function Input({ message, setMessage }: InputProps) {
 	};
 
 	useEffect(() => {
-		textareaRef.current?.focus();
-	}, []);
+		if (autoFocus) {
+			textareaRef.current?.focus();
+		}
+	}, [autoFocus]);
 
 	return (
 		<textarea
@@ -37,6 +46,7 @@ export default function Input({ message, setMessage }: InputProps) {
 			onInput={handleInput}
 			onKeyDown={handleKeyDown}
 			onChange={(e) => setMessage(e.target.value)}
+			disabled={disabled}
 			rows={1}
 			placeholder="Type what you want to listen to..."
 			className="w-100 pr-12 pl-6 py-3 bg-gray-100 rounded-full flex-1 resize-none shadow-sm overflow-hidden focus:outline-none focus:ring-2 focus:ring-gray-700"
