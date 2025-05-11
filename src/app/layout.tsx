03c3a3e317409bb2c96d018931b1250a1a4e4c85
ismaelpaul <ismaafp@gmail.com>
2025-05-11 16:45:41 +0100

    Add AuthProvider, SpotifyPlayerProvider and zain font family

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,19 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import { Zain } from 'next/font/google';
+
+import { AuthProvider } from '@/context/AuthContext';
+import { SpotifyPlayerProvider } from '@/context/SpotifyPlayerContext';
 
 export const metadata: Metadata = {
 	title: 'MusicMate',
 	description: 'Your music recommendations mate',
 };
+
+const zain = Zain({
+	subsets: ['latin'],
+	weight: ['300', '400', '700', '900'],
+});
 
 export default function RootLayout({
 	children,
@@ -12,8 +21,12 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
-			<body>{children}</body>
+		<html lang="en" className={zain.className}>
+			<body>
+				<AuthProvider>
+					<SpotifyPlayerProvider>{children}</SpotifyPlayerProvider>
+				</AuthProvider>
+			</body>
 		</html>
 	);
 }
