7ce9f5ee84aeec3eed1181e4c59c6ec14dda4edf
ismaelpaul <ismaafp@gmail.com>
2025-05-13 20:37:54 +0100

    Update styling

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,17 +1,15 @@
-import Form from '@/components/Form';
-import SpotifyLoginButton from '@/components/SpotifyLoginButton';
+import SpotifyLoginButton from '@/components/Buttons/SpotifyLoginButton';
+import Form from '@/components/Form/Form';
 
 export default function Login() {
 	return (
-		<main className="flex flex-col justify-end min-h-screen py-24 px-16 lg:px-56">
+		<main className="flex flex-col justify-end min-h-screen">
 			<div className="absolute inset-0 z-10 bg-black/20 backdrop-blur-[2px]" />
 			<div className="flex flex-col gap-4 absolute inset-0 z-20 items-center justify-center">
-				<h1 className="text-2xl font-bold text-black">
-					Please login to continue
-				</h1>
+				<h1 className="text-3xl text-black">Please login to continue</h1>
 				<SpotifyLoginButton />
 			</div>
-			<div>
+			<div className="w-3xl max-w-3xl p-6 mx-auto">
 				<Form autoFocus={false} disabled={true} />
 			</div>
 		</main>
