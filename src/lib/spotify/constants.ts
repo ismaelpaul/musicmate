e70c295f9a6d2bcd2e3cfced5b03316773b7bb4e
ismaelpaul <ismaafp@gmail.com>
2025-05-14 18:09:34 +0100

    Add more scope options

--- src/lib/spotify/constants.ts
+++ src/lib/spotify/constants.ts
@@ -1,7 +1,7 @@
 export const STATE_KEY = 'spotify_auth_state';
 export const TOKEN_COOKIE_KEY = 'spotify_access_token';
 export const SCOPE =
-	'user-read-private user-read-email streaming user-modify-playback-state';
+	'user-read-private user-read-email streaming user-modify-playback-state user-read-playback-state app-remote-control';
 
 export const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID!;
 export const SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET!;
