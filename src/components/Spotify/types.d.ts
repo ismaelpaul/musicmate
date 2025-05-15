9267865204494d28e8444df4ab8830305adf7512
ismaelpaul <ismaafp@gmail.com>
2025-05-15 09:57:49 +0100

    Add spotify types

---
+++ src/components/Spotify/types.d.ts
@@ -0,0 +1,58 @@
+interface SpotifyExternalUrls {
+	spotify: string;
+}
+
+interface SpotifyArtist {
+	external_urls: SpotifyExternalUrls;
+	href: string;
+	id: string;
+	name: string;
+	type: string;
+	uri: string;
+}
+
+interface SpotifyImage {
+	height: number;
+	url: string;
+	width: number;
+}
+
+interface SpotifyAlbum {
+	album_type: 'album' | 'single' | 'compilation';
+	artists: SpotifyArtist[];
+	external_urls: SpotifyExternalUrls;
+	href: string;
+	id: string;
+	images: SpotifyImage[];
+	is_playable: boolean;
+	name: string;
+	release_date: string;
+	release_date_precision: 'year' | 'month' | 'day';
+	total_tracks: number;
+	type: string;
+	uri: string;
+}
+
+interface SpotifyExternalIds {
+	isrc?: string;
+}
+
+export interface RecommendationTrack {
+	album: SpotifyAlbum;
+	artists: SpotifyArtist[];
+	disc_number: number;
+	duration_ms: number;
+	explicit: boolean;
+	external_ids: SpotifyExternalIds;
+	external_urls: SpotifyExternalUrls;
+	href: string;
+	id: string;
+	is_local: boolean;
+	is_playable: boolean;
+	name: string;
+	popularity: number;
+	preview_url: string | null;
+	track_number: number;
+	type: string;
+	uri: string;
+}
