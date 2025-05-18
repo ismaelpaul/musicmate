23298ca6b07e22489ac46d38ef1e86d164cd46fd
ismaelpaul <ismaafp@gmail.com>
2025-05-18 14:47:21 +0100

    Add SpotifyPlayer component

---
+++ src/components/Spotify/SpotifyPlayer.tsx
@@ -0,0 +1,34 @@
+import SpotifyProgressBar from './SpotifyProgressBar';
+import SpotifyPlayerControls from './SpotifyPlayerControls';
+
+import {
+	useSpotifyPlayerActions,
+	useSpotifyPlayerState,
+} from '@/context/SpotifyPlayerContext';
+import SpotifyTrackInfo from './SpotifyTrackInfo';
+
+export default function SpotifyPlayer() {
+	const { seek, togglePlay } = useSpotifyPlayerActions();
+	const { duration, position, isPaused, currentTrackPlaying } =
+		useSpotifyPlayerState();
+
+	if (!currentTrackPlaying) return;
+
+	return (
+		<div
+			className="w-full p-6 text-white rounded-lg shadow-lg bg-cover bg-center mt-70"
+			style={{
+				backgroundImage: `linear-gradient(to top, rgba(0,0,0,0.6), rgba(0,0,0,0.2)), url(${currentTrackPlaying.album.images[0].url})`,
+			}}
+		>
+			<SpotifyTrackInfo track={currentTrackPlaying} />
+
+			<SpotifyProgressBar
+				position={position}
+				duration={duration}
+				onSeek={seek}
+			/>
+			<SpotifyPlayerControls isPaused={isPaused} onTogglePlay={togglePlay} />
+		</div>
+	);
+}
