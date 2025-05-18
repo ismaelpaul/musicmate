35ac0ab96049d9d42227ae2574b2b86474820f74
ismaelpaul <ismaafp@gmail.com>
2025-05-18 14:45:50 +0100

    Add sidebar-items array

---
+++ src/components/Sidebar/sidebar-items.ts
@@ -0,0 +1,11 @@
+import { HiOutlineCog6Tooth } from 'react-icons/hi2';
+import { RecommendationLimitSlider } from '../Recommendations/RecommendationLimitSlider';
+
+export const SIDEBAR_ITEMS = [
+	{
+		id: 'config',
+		label: 'Configurations',
+		icon: HiOutlineCog6Tooth,
+		content: RecommendationLimitSlider,
+	},
+];
