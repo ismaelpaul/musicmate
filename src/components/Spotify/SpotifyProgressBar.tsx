5b3b5203d2d2654e90d0fce39dae5b8cd3df1ce5
ismaelpaul <ismaafp@gmail.com>
2025-05-30 17:40:19 +0100

    Update styling

--- src/components/Spotify/SpotifyProgressBar.tsx
+++ src/components/Spotify/SpotifyProgressBar.tsx
@@ -10,7 +10,7 @@ export default function SpotifyProgressBar({
 	onSeek: (ms: number) => void;
 }) {
 	return (
-		<div className="w-full mt-2">
+		<div className="flex-1 mt-2">
 			<input
 				type="range"
 				min={0}
@@ -19,7 +19,7 @@ export default function SpotifyProgressBar({
 				onChange={(e) => onSeek(Number(e.target.value))}
 				className="w-full"
 			/>
-			<div className="flex justify-between text-xs text-gray-400 mt-1">
+			<div className="flex justify-between text-sm text-white mt-1">
 				<span>{formatTime(position)}</span>
 				<span>{formatTime(duration)}</span>
 			</div>
