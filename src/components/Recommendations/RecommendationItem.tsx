4f3c5329a37dbec407d03bc4803ec66a48553087
ismaelpaul <ismaafp@gmail.com>
2025-05-31 09:27:14 +0100

    Refactor RecommendationItem to use the props playTrack, togglePlay, currentTrackUri, deviceId and isPaused

--- src/components/Recommendations/RecommendationItem.tsx
+++ src/components/Recommendations/RecommendationItem.tsx
@@ -1,21 +1,53 @@
-import { CirclePause, PlayCircle } from 'lucide-react';
-import IconButton from '../IconButton';
+import { FaPause } from 'react-icons/fa6';
+import IconButton from '../Buttons/IconButton';
 import Image from 'next/image';
 import { RecommendationTrack } from '../Spotify/types';
+import { FaPlay } from 'react-icons/fa';
+import { useSidebarStore } from '@/store/useSidebarStore';
 
 interface RecommendationItemProps {
 	track: RecommendationTrack;
-	isPlaying: boolean;
-	onPlay: () => void;
+	playTrack: (track: RecommendationTrack) => Promise<void>;
+	currentTrackUri: string | null;
+	deviceId: string | null;
+	togglePlay: () => void;
+	isPaused: boolean;
 }
 
 export function RecommendationItem({
 	track,
-	isPlaying,
-	onPlay,
+	playTrack,
+	currentTrackUri,
+	deviceId,
+	togglePlay,
+	isPaused,
 }: RecommendationItemProps) {
+	const { setIsExpanded } = useSidebarStore();
+	const isThisTrackTheCurrentOne = currentTrackUri === track.uri;
+
+	const isThisTrackPlayingActive = isThisTrackTheCurrentOne && !isPaused;
+
+	const handlePlaySpecificTrack = () => {
+		if (deviceId) {
+			setIsExpanded(true);
+			playTrack(track);
+		} else {
+			console.warn(
+				'RecommendationItem: Play clicked, but deviceId is not available.'
+			);
+		}
+	};
+
+	const handleToggleCurrentTrack = () => {
+		togglePlay();
+	};
+
 	return (
-		<div className="flex justify-between items-center p-2 bg-white rounded-lg">
+		<div
+			className={`flex justify-between items-center p-2 rounded-lg bg-white ${
+				isThisTrackTheCurrentOne ? 'border drop-shadow-md' : ''
+			} hover:bg-gray-200 transition-colors duration-200`}
+		>
 			<div className="flex items-center gap-3">
 				<Image
 					src={track.album.images[0].url}
@@ -24,7 +56,7 @@ export function RecommendationItem({
 					height={50}
 					className="rounded-full"
 				/>
-				<div className="flex flex-col">
+				<div className="flex flex-col text-lg">
 					<p className="text-black font-bold">{track.name}</p>
 					<div className="flex gap-2">
 						{track.artists.map((artist, index) => (
@@ -37,9 +69,14 @@ export function RecommendationItem({
 				</div>
 			</div>
 			<IconButton
-				className="cursor-pointer"
-				onClick={onPlay}
-				icon={isPlaying ? <CirclePause /> : <PlayCircle />}
+				className="flex items-center justify-center bg-black p-3 rounded-full cursor-pointer text-white hover:bg-green-spotify"
+				onClick={
+					isThisTrackTheCurrentOne
+						? handleToggleCurrentTrack
+						: handlePlaySpecificTrack
+				}
+				disabled={!deviceId}
+				icon={isThisTrackPlayingActive ? <FaPause /> : <FaPlay />}
 			/>
 		</div>
 	);
