d65252103c70bf8ea932e4f8a7f27595c35f5d59
ismaelpaul <ismaafp@gmail.com>
2025-06-04 16:35:33 +0100

    Refactor page with layout component and messaging

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,54 @@
-import Form from '@/components/Form';
-import SpotifyLoginButton from '@/components/SpotifyLoginButton';
+'use client';
+
+import Form from '@/components/Form/Form';
+import Layout from '@/components/Layout/Layout';
+import { MessagesList } from '@/components/Messages/MessagesList';
+import { SystemMessage, UserMessage } from '@/components/Messages/types';
+
+import { RecommendationTrack } from '@/components/Spotify/types';
+
+import { useCallback, useState } from 'react';
+
+type Messages = UserMessage | SystemMessage;
 
 export default function Home() {
+	const [messages, setMessages] = useState<Messages[]>([]);
+	console.log('messages', messages);
+
+	// ddd a new message
+	const handleSendMessage = useCallback((newMessage: Messages) => {
+		setMessages((prevMessages) => [...prevMessages, newMessage]);
+	}, []);
+
+	// update system message with results or error
+	const handleResultsReceived = useCallback(
+		(
+			messageId: string,
+			recommendations: RecommendationTrack[],
+			status: 'success' | 'error',
+			error?: string
+		) => {
+			setMessages((prevMessages) =>
+				prevMessages.map((msg) =>
+					msg.id === messageId && msg.role === 'system'
+						? { ...msg, status, recommendations, error }
+						: msg
+				)
+			);
+		},
+		[]
+	);
+
 	return (
-		<main className="flex flex-col justify-end min-h-screen py-24 px-16 lg:px-56">
-			<SpotifyLoginButton />
-			<Form />
-		</main>
+		<Layout>
+			{/* main content */}
+			<div className="flex-1 overflow-y-auto no-scrollbar">
+				<MessagesList messages={messages} />
+			</div>
+			<Form
+				onSendMessage={handleSendMessage}
+				onResultsReceived={handleResultsReceived}
+			/>
+		</Layout>
 	);
 }
