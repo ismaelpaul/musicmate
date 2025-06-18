fc743c7bd6055c446ef153b0fbdbb3af3b2de061
ismaelpaul <ismaafp@gmail.com>
2025-06-18 15:34:32 +0100

    Update input styling

--- src/components/Inputs/TextInput.tsx
+++ src/components/Inputs/TextInput.tsx
@@ -25,7 +25,7 @@ export const TextInput = ({
 				placeholder={placeholder}
 				value={value}
 				onChange={onChange}
-				className="block w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+				className="block bg-white w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-black focus:border-black sm:text-sm"
 			/>
 		</div>
 	</div>
